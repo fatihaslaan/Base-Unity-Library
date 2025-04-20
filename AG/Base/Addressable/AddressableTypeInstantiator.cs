fc0d2cc42af84d469f2209a033409ad21e9302fb
Fatih Aslan <44754892+fatihaslaan@users.noreply.github.com>
2025-04-20 18:01:06 +0300

    Update AddressableTypeInstantiator.cs

--- AG/Base/Addressable/AddressableTypeInstantiator.cs
+++ AG/Base/Addressable/AddressableTypeInstantiator.cs
@@ -5,17 +5,17 @@ namespace AG.Base.Addressable
     //Gets Desired Type From Instantiated Addressable GameObject
     public abstract class AddressableTypeInstantiator<TResult> : AddressableInstantiator where TResult : MonoBehaviour
     {
-        protected TResult operationTResult = default;
+        protected new TResult operationResult = default;
 
         protected override GameObject FinalizeOperationResult(GameObject result)
         {
             if (result.TryGetComponent(out TResult tempResult))
             {
-                operationTResult = tempResult;
+                operationResult = tempResult;
             }
             else if (AllowComponentAddition())
             {
-                operationTResult = result.AddComponent<TResult>();
+                operationResult = result.AddComponent<TResult>();
                 Debug.Log("No Component Found On Instantiated Result, Added One");
             }
             else
